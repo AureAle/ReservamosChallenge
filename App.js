f1fbb11d1defb3466d22d65ee6029ed93ea04959
AureAle <aureale@AureAles-MacBook-Pro.local>
2024-05-22 16:37:27 -0600

    Delete unnecesary code

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
-import React, { useEffect, useState, useNa } from "react";
-import {
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import City from "./City.js";
-import Home from "./Home.js";
+import City from "./src/screens/City.js";
+import Home from "./src/screens/Home.js";
 
 const Stack = createNativeStackNavigator();
 
@@ -24,12 +14,10 @@ const ExampleAPIs = () => {
         initialRouteName="Home"
         screenOptions={{
           headerShown: false,
-         
         }}
       >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="City" component={City} />
-        
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -43,50 +31,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  item: {
-    backgroundColor: "#f5f5f5",
-    padding: 10,
-    marginVertical: 8,
-    borderRadius: 8,
-  },
-  header: {
-    alignSelf: "center",
-    fontSize: 30,
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "blue",
-    fontSize: 25,
-    padding: 5,
-  },
-  title: {
-    alignSelf: "center",
-    flex: 1,
-    justifyContent: "center",
-  },
-  min: {
-    backgroundColor: "#CAF4FF",
-    padding: 10,
-    borderRadius: 5,
-    margin: 10,
-  },
-  textMin: {
-    fontSize: 20,
-    color: "black",
-  },
-  max: {
-    backgroundColor: "#F28585",
-    padding: 10,
-    borderRadius: 5,
-    margin: 10,
-  },
-  textMax: {
-    fontSize: 20,
-    color: "black",
-  },
-  temps: {
-    flexDirection: "row",
-    justifyContent: "space-between",
   },
 });
